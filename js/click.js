4c169262d49ab4ce727e189f04741ea2e01935e4
Esperanza Amaya <eamayag@gmail.com>
2018-09-03 19:15:48 +0200

    first preversion

--- js/click.js
+++ js/click.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){ 
 
-  $("#start").click(function(){
+  $("#start").click(function(){ //Start button calls a new bubble and scoring
     console.log("Start");  
-   newBubbles();
+    newBubbles();
   })
 
   var coloursBall = ["red", "green", "blue"];
 
-  function newBubbles() {
+  function newBubbles() {   //generates new bubble with random properties and set timer
     $("#game").append("<div>Click me!</div>");
     $("div").addClass("ball");
-    var randomWidth = Math.floor(Math.random() * (1024));
+    var randomWidth = Math.floor(Math.random() * (1024)); //max width on css 1024px 
     if (randomWidth > 1024 - $(".ball").width) {
       randomWidth /= 2;
     }
     console.log(randomWidth);
-    var randomHeight = Math.floor(Math.random() * (960));
+    var randomHeight = Math.floor(Math.random() * (960)); //max heigth on css 960px
     if (randomHeight > 960 - $("ball").height) {
       randomHeight /= 2
     }
@@ -26,13 +26,31 @@ $(document).ready(function(){
     $(".ball").css({"margin-left": randomWidth});
     $(".ball").css({"margin-top": randomHeight});
     $(".ball").css({"background-color": randomColor});
+    
+  //  var timeoutID = setTimeout(function(){gameOver()},2000) //timer to click the bubble  
   }
 
-
-  $("#game").click(function(){
+  var click = $("#game").click(function(){ //click a bubble call to end previous bubble and timer
     console.log("Ball");  
-   newBubbles();
+    //clearTimeout(timeoutID);
+    newBubbles();
   })
 
+  function gameOver() { //to call if time is out
+    console.log("hello")
+    var gameOver = "You lost!";
+    console.log(gameOver);
+  }
+
 })
+
+
+// var click = $("#game").click(function(){ //click a bubble call to end previous bubble and timer
+//   console.log("Ball");  
+//   var timer = setTimeout(function() { $("#game").click(function(){newBubbles()}); }, 2000);
+//   $("#game").click(function(){
+//     clearTimeout(timer);
+//     gameOver();
+// })
+// });
 
