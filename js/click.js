1e06b64d04a980c1e73010f19cfc9bc2139a0a01
Esperanza Amaya <eamayag@gmail.com>
2018-09-03 15:14:33 +0200

    first random movement

--- js/click.js
+++ js/click.js
@@ -8,8 +8,15 @@ $(document).ready(function(){
   function newBubbles() {
     $("#game").append("<div>Hello</div>");
     $("div").addClass("ball");
-  }
+    $("div").addClass("new-pos");
+    var randomWidth = Math.floor(Math.random() * (1000));
+    console.log(randomWidth);
+    var randomHeight = Math.floor(Math.random() * (1000));
+    console.log(randomHeight);
 
+    $(".ball").css({"margin-left": randomWidth});
+    $(".ball").css({"margin-top": randomHeight});
+  }
 
 })
 
