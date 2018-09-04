87dd56266a3de918f2375a02d39b35cf4c26d2a6
Esperanza Amaya <eamayag@gmail.com>
2018-09-04 19:16:40 +0200

    hardcore level added, expecting to implement; 3levels-game functioninggit status

--- js/click.js
+++ js/click.js
@@ -30,6 +30,52 @@ $(document).ready(function() {
     score.innerHTML = "Your score: " + counter; 
   }
 
+  function mediumBubbles() {
+    //generates new bubble with random properties and set timer
+    var randomWidth = Math.floor(Math.random() * (1024 - $(".ball").width())); //max width on css 1024px
+    console.log(randomWidth);
+    var randomHeight = Math.floor(Math.random() * (960 - $(".ball").height())); //max heigth on css 960px
+    console.log(randomHeight);
+    var randomColor = coloursBall[Math.floor(Math.random() * coloursBall.length)];
+    console.log(randomColor);
+
+    $(".ball").css({ "margin-left": randomWidth });
+    $(".ball").css({ "margin-top": randomHeight });
+    $(".ball").css({ "background-color": randomColor });
+
+    timeoutID = setTimeout(function() {
+      gameOver();
+    }, 1000); //timer to click the bubble
+    
+    counter++ //number of clicks, for scoring
+    console.log(counter)
+    var score = document.getElementById("counter");
+    score.innerHTML = "Your score: " + counter; 
+  }
+
+  function hardBubbles() {
+    //generates new bubble with random properties and set timer
+    var randomWidth = Math.floor(Math.random() * (1024 - $(".ball").width())); //max width on css 1024px
+    console.log(randomWidth);
+    var randomHeight = Math.floor(Math.random() * (960 - $(".ball").height())); //max heigth on css 960px
+    console.log(randomHeight);
+    var randomColor = coloursBall[Math.floor(Math.random() * coloursBall.length)];
+    console.log(randomColor);
+
+    $(".ball").css({ "margin-left": randomWidth });
+    $(".ball").css({ "margin-top": randomHeight });
+    $(".ball").css({ "background-color": randomColor });
+
+    timeoutID = setTimeout(function() {
+      gameOver();
+    }, 500); //timer to click the bubble
+    
+    counter++ //number of clicks, for scoring
+    console.log(counter)
+    var score = document.getElementById("counter");
+    score.innerHTML = "Your score: " + counter; 
+  }
+
   function gameOver() {
     //to call if time is out
     lost = true;
@@ -76,35 +122,17 @@ $(document).ready(function() {
     lost = false;
     console.log("Start");
     counter = 0;
-    newBubbles();
+    mediumBubbles();
   });
   
 
   $("#hard").click(function() {
     $("#level").empty();
-    $("#level").append("<div>Pray for your soul</div>");
+    $("#level").append("<div>Get ready to run!!</div>");
     $("#start").empty();
     $("#start").append("<button id='hardGame'>Let's blaster!</button>");
   });
 
-  function makeNewPosition(){ //moving ball for hardcore level
-      var h = $(window).height() - 50;
-      var w = $(window).width() - 50;
-    
-      var nh = Math.floor(Math.random() * h);
-      var nw = Math.floor(Math.random() * w);
-    
-      return [nh,nw];      
-    }
-  
-    function movingDiv(element){
-      var newq = makeNewPosition();
-      $(element).animate({ 
-        top: newq[0], left: newq[1] }, 1000, function(){
-        movingDiv(element);        
-        });
-    };
-  
   $("#start").on("click", "#hardGame", function() {
     //Start button calls a new bubble and scoring
     $(".ball").remove();
@@ -112,8 +140,53 @@ $(document).ready(function() {
     lost = false;
     console.log("Start");
     counter = 0;
-    newBubbles();
-    movingDiv();
+    hardBubbles();
   });
+
+  $("#hardcore").click(function() {
+    $("#level").empty();
+    $("#level").append("<div>PRAY FOR YOUR SOUL</div>");
+    $("#start").empty();
+    $("#start").append("<button id='hardcoreGame'>Let's blaster!</button>");
+  });
+
+  $("#start").on("click", "#hardcoreGame", function() {
+    //Start button calls a new bubble and scoring
+    $(".ball").remove();
+    $("#game").append("<div class='ball'></div>");
+    lost = false;
+    console.log("Start");
+    counter = 0;
+    hardcoreBubbles();
+  });
+
+  // function makeNewPosition(){ //moving ball for hardcore level
+  //     var h = $(window).height() - 50;
+  //     var w = $(window).width() - 50;
+    
+  //     var nh = Math.floor(Math.random() * h);
+  //     var nw = Math.floor(Math.random() * w);
+    
+  //     return [nh,nw];      
+  //   }
+  
+  //   function movingDiv(element){
+  //     var newq = makeNewPosition();
+  //     $(element).animate({ 
+  //       top: newq[0], left: newq[1] }, 1000, function(){
+  //       movingDiv(element);        
+  //       });
+  //   };
+  
+  // $("#start").on("click", "#hardGame", function() {
+  //   //Start button calls a new bubble and scoring
+  //   $(".ball").remove();
+  //   $("#game").append("<div class='ball'></div>");
+  //   lost = false;
+  //   console.log("Start");
+  //   counter = 0;
+  //   newBubbles();
+  //   movingDiv();
+  // });
 
 });
